85af1395f0d7386f657bacef2512b53f4a74abc5
Dahoe Shin <kity990304@gmail.com>
2016-05-26 20:13:09 +0900

    #2 Event complete

--- app/src/main/java/kr/hs/emirim/dahoe0304/imageslideshow/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dahoe0304/imageslideshow/MainActivity.java
@@ -2,12 +2,40 @@ package kr.hs.emirim.dahoe0304.imageslideshow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    ViewFlipper flip;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        flip = (ViewFlipper) findViewById(R.id.view_flip);    //강제형변환은 (viewFlipper)
+        flip.setFlipInterval(1000); //1초니깐 1000.
+
+        Button but_start=(Button)findViewById(R.id.but_start);
+        but_start.setOnClickListener(this);
+        Button but_stop=(Button)findViewById(R.id.but_stop);
+        but_stop.setOnClickListener(this);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.but_start:
+                flip.startFlipping();
+                break;
+            case R.id.but_stop:
+                flip.stopFlipping();
+                break;
+        }
     }
 }
